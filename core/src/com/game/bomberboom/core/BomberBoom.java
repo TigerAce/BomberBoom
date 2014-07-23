1f70dcbb9a68b71d45a337bbef866dcfc4e79760
lit0908 <tongli0809@gmail.com>
2014-07-23 23:45:03 +1000

    Re-factoring code
    Dont remember what I did. Some code re-factoring.

--- core/src/com/game/bomberboom/core/BomberBoom.java
+++ core/src/com/game/bomberboom/core/BomberBoom.java
@@ -1,16 +1,70 @@
 package com.game.bomberboom.core;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.game.bomberboom.screen.GamePlay;
 import com.game.bomberboom.screen.Splash;
 
 
 public class BomberBoom extends Game {
 
-	
+    /**
+     * Define different game state
+     */
+	public static final int SPLASH_SCREEN_STATE = 0;
+    public static final int GAME_PLAY_STATE = 1;
+
+    /**
+     * Set the initial game state to -1, so that it won't math any of the states declared above.
+     * If gameState is -1 after game started, then something bad is happening
+     */
+    private int gameState = -1;
+
 	@Override
 	public void create () {
+
+        /**
+         * Keyboard bindings
+         */
+        Gdx.input.setInputProcessor(new InputController(){
+            @Override
+            public boolean keyUp(int keycode){
+
+                switch (gameState) {
+                    case BomberBoom.SPLASH_SCREEN_STATE:
+                        /**
+                         * KeyUp bindings for splash screen state
+                         */
+                        switch (keycode) {
+                            case Input.Keys.ESCAPE:
+                                // Skip the splash screen and go into GamePlay screen
+                                ((Game)Gdx.app.getApplicationListener()).setScreen(new GamePlay());
+                                break;
+                        }
+                        break;
+
+                    case BomberBoom.GAME_PLAY_STATE:
+                        /**
+                         * KeyUp bindings for in-game state
+                         */
+                        switch (keycode) {
+                            case Input.Keys.ESCAPE:
+                                // TODO
+                                // Now it exits the game immediately, how about change it to pause?
+                                System.exit(1);
+                                break;
+                        }
+                }
+                return true;
+            }
+        });
+
+        /**
+         * A splash screen will be loaded when game starts
+         * GamePlay screen will be loaded after this Splash screen, which is implemented inside Splash.show()
+         */
         setScreen(new Splash());
-		//setScreen(new GamePlay());
 	}
 
 	@Override
@@ -33,4 +87,11 @@ public class BomberBoom extends Game {
 		super.dispose();
 	}
 
+    public int getGameState() {
+        return gameState;
+    }
+
+    public void setGameState(int gameState) {
+        this.gameState = gameState;
+    }
 }
--- core/src/com/game/bomberboom/screen/Splash.java
+++ core/src/com/game/bomberboom/screen/Splash.java
@@ -4,23 +4,20 @@ import aurelienribon.tweenengine.BaseTween;
 import aurelienribon.tweenengine.Tween;
 import aurelienribon.tweenengine.TweenCallback;
 import aurelienribon.tweenengine.TweenManager;
-
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.game.bomberboom.core.InputController;
+import com.game.bomberboom.core.BomberBoom;
 import com.game.bomberboom.tween.SpriteAccessor;
-import com.badlogic.gdx.Game;
 
 /**
- * Created by toli9_000 on 2014/7/20.
+ * Created by Tong on 2014/7/20.
  */
-
-public class Splash implements Screen{
+public class Splash implements Screen {
 
     private SpriteBatch batch;
     private Sprite splash;
@@ -29,6 +26,7 @@ public class Splash implements Screen{
     @Override
     public void render(float delta) {
 
+        // This would basically just clear the screen to black
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -47,40 +45,33 @@ public class Splash implements Screen{
     @Override
     public void show() {
 
+        // Set the current game state to BomberBoom.SPLASH_SCREEN_STATE
+        ((BomberBoom) Gdx.app.getApplicationListener()).setGameState(BomberBoom.SPLASH_SCREEN_STATE);
+
         batch = new SpriteBatch();
         tweenManager = new TweenManager();
-        /**
-         * skip sprite
-         */
-        Gdx.input.setInputProcessor(new InputController(){
-			public boolean keyUp(int keycode){
-				switch(keycode){
-				case Keys.ESCAPE:
-					 ((Game)Gdx.app.getApplicationListener()).setScreen(new GamePlay());
-					
-					break;
-				}
-				return true;
-			}
-        });
-        
+
         Tween.registerAccessor(Sprite.class, new SpriteAccessor());
         Texture splashTexture = new Texture(Gdx.files.internal("img/splash/bomb_splash_screen.jpg"));
-      //  Texture splashTexture = new Texture("img/bomb_splash_screen.jpg");
         splash = new Sprite(splashTexture);
         splash.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
+        /**
+         * Make the splash screen animate
+         * It would just simply fade in and then fade out
+         */
         Tween.set(splash, SpriteAccessor.ALPHA).target(0).start(tweenManager);
         Tween.to(splash, SpriteAccessor.ALPHA, 2).target(1).repeatYoyo(1, 2).setCallback(
                 new TweenCallback() {
                     @Override
                     public void onEvent(int i, BaseTween<?> baseTween) {
-                        ((Game)Gdx.app.getApplicationListener()).setScreen(new GamePlay());
+                        /*
+                         * Move to GamePlay screen and ready to play the game
+                         */
+                        ((Game) Gdx.app.getApplicationListener()).setScreen(new GamePlay());
                     }
                 }
         ).start(tweenManager);
-        
-       
     }
 
     @Override
